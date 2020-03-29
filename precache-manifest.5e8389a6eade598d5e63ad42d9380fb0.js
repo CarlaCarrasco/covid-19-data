78a7a903863c3d5a4f9e122f13b484cc6c1c6e16
CarlaCarrasco <carlacarr37@gmail.com>
2020-03-29 14:38:11 -0500

    Updates

---
+++ precache-manifest.5e8389a6eade598d5e63ad42d9380fb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57f81e4ef57226387deb0277a08abd23",
+    "url": "/covid-19-data/index.html"
+  },
+  {
+    "revision": "3ea52a1de234c7bc8f53",
+    "url": "/covid-19-data/static/css/main.c75e1f96.chunk.css"
+  },
+  {
+    "revision": "ebed3b41a120d61d3b67",
+    "url": "/covid-19-data/static/js/2.d3c636d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19-data/static/js/2.d3c636d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ea52a1de234c7bc8f53",
+    "url": "/covid-19-data/static/js/main.0d2b8aab.chunk.js"
+  },
+  {
+    "revision": "ecf88510f1ba84ddd1c6",
+    "url": "/covid-19-data/static/js/runtime-main.0139bb0e.js"
+  }
+]);
